39afe10a3be2b64aaf9f99ba4e602579ff89f271
stas <stasma@gmail.com>
2019-02-15 15:36:50 +0200

    Initial commit

---
+++ SimpleProjects/googlesearch.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("../drivers/chromedriver.exe")
+
+driver.get("http://google.com")
+
+time.sleep(5)
+
+driver.quit()
