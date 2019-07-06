bcc3940ef2d784faf6e5475a5e8b6c447715a356
Totti10as <stasma@gmail.com>
2019-07-06 18:21:20 +0300

    Update test_allure.py

--- allure_project/test_allure.py
+++ allure_project/test_allure.py
@@ -31,15 +31,15 @@ class TestGoogleSearch:
         assert driver.title == 'Suzuki - Google Search'
 
 
-    # @allure.feature('Search')
-    # @allure.story('Open google.com and search for Stas')
-    # @allure.severity('blocker')
-    # def test_search_stas(self):
-    #     self.driver.get("https://www.google.com/?gl=us&hl=en&pws=0&gws_rd=cr")
-    #     self.driver.find_element_by_name("q").send_keys("Stas")
-    #     self.driver.find_element_by_name("btnK").submit()
-    #     with allure.step('Get screenshot'):
-    #         allure.attach(self.driver.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
+    @allure.feature('Search')
+    @allure.story('Open google.com and search for Stas')
+    @allure.severity('blocker')
+    def test_search_stas(self):
+        self.driver.get("https://www.google.com/?gl=us&hl=en&pws=0&gws_rd=cr")
+        self.driver.find_element_by_name("q").send_keys("Stas")
+        self.driver.find_element_by_name("btnK").submit()
+        with allure.step('Get screenshot'):
+            allure.attach(self.driver.get_screenshot_as_png(), name='Screenshot', attachment_type=AttachmentType.PNG)
 
 
     # def test_teardown():
